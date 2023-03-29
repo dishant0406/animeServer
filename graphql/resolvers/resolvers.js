f46076e889fef523a70517190bc4b05bd498dcc3
Dishant Sharma <dishu5570@gmail.com>
2023-03-30 01:06:28 +0530

    remodeled the API

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -1,110 +1,76 @@
 import axios from 'axios'
 import malScraper from 'mal-scraper'
-import { ANIME, StreamingServers } from '@consumet/extensions';
-const gogo = new ANIME.Gogoanime()
+
+const URL = `http://localhost:3000`
 
 const Query = {
   topAnime: async (_, args) => {
     const page = args.page || 0
-    const d = await gogo.fetchTopAiring(page)
-    return d.results
+    const res = await axios.get(URL + `/top-airing/?page=${page}`)
+    res.data = res.data.map((anime) => {
+      anime.id = anime.animeId
+      anime.title = anime.animeTitle
+      anime.imgUrl = anime.animeImg
+      delete anime.animeId
+      delete anime.animeTitle
+      delete anime.animeImg
+      return anime
+    })
+    const promises = res.data.map(async (anime) => {
+      const d = await malScraper.getInfoFromName(anime.id)
+      anime.imgUrl = anime.imgUrl || d.picture
+      anime.trailer = d.trailer
+      return anime
+    })
+
+    res.data = await Promise.all(promises)
+    return res.data
   },
   animeSearch: async (parent, args) => {
-    const page = args.page || 0
-    const d = await gogo.search(args.id, page)
-    return d.results
+    const page = args.page || 1
+    let query = args.id
+    if (query.includes(' ')) {
+      query = query.split(' ').join('+')
+    }
+    const res = await axios.get(URL + `/search?keyw=${query}&page=${page}`)
+    res.data = res.data.map((anime) => {
+      anime.id = anime.anime_id
+      anime.title = anime.name
+      anime.imgUrl = anime.img_url
+      delete anime.animeId
+      delete anime.name
+      delete anime.img_url
+      return anime
+    }
+    )
+    return res.data
+
   },
   animeDetails: async (parent, args) => {
-    const data = await gogo.fetchAnimeInfo(args.id)
-    const d = await malScraper.getInfoFromName(data.id)
-    data.image = data.image || d.picture
-    data.trailer = d.trailer
-    return data
+    const data = await axios.get(URL + `/getAnime/${args.id}`)
+    const d = await malScraper.getInfoFromName(args.id)
+    data.data.image = data.data.image || d.picture
+    data.data.trailer = d.trailer
+    data.data.id = args.id
+    return data.data
   },
   streamLinkDetails: async (parent, args) => {
-    const data = await gogo.fetchEpisodeSources(args.id)
-
-    return data.sources.map((source) => {
-      return ({
-        id: `${args.id}-${source.quality}`,
-        url: source.url,
-        quality: source.quality,
-        isM3U8: source.isM3U8,
-        episodeId: args.id,
-      })
-    });
+    const data = await axios.get(URL + `/getEpisode/${args.id}`)
+    console.log(data.data)
+    data.data.hls = data.data.gogoSourcesHLS?.[0]?.file || data.data.gogoSourcesBackupHLS?.[0]?.file
+    data.data.id = args.id
+    return data.data
   },
   recentEpisodes: async (parent, args) => {
-
-
     const page = args.page || 0
-    const d = await gogo.fetchRecentEpisodes(page)
-
-
-    const dataToSend = d.results.map((recent) => ({
-      animeId: recent.id,
-      episodeId: recent.episodeId,
-      episodeNumber: recent.episodeNumber,
-      animeTitle: recent.title,
-      image: recent.image,
-      animeUrl: recent.url,
-    }))
-
-    return {
-      results: dataToSend,
-      hasNextPage: d.hasNextPage
-    }
+    const res = await axios.get(URL + `/recent-release/?page=${page}`)
+    return res.data
   }
 
 }
 
-const Recent = {
-  anime: async (parent, args) => {
-    const data = await gogo.fetchAnimeInfo(parent.animeId)
-    const d = await malScraper.getInfoFromName(data.id)
-    data.image = data.image || d.picture
-    data.trailer = d.trailer
-    return data
-  },
-  streamLinks: async (parent, args) => {
-    const data = await gogo.fetchEpisodeSources(parent.episodeId)
-    return data.sources.map((source) => ({
-      id: `${parent.episodeId}-${source.quality}`,
-      url: source.url,
-      quality: source.quality,
-      isM3U8: source.isM3U8,
-      episodeId: parent.episodeId,
-    }));
-  }
 
-}
-
-const Anime = {
-  animeDetails: async (parent, args) => {
-    const data = await gogo.fetchAnimeInfo(parent.id)
-    const d = await malScraper.getInfoFromName(data.id)
-    data.image = data.image || d.picture
-    data.trailer = d.trailer
-    return data
-  }
-}
-
-const Episode = {
-  streamLinks: async (parent, args) => {
-    const data = await gogo.fetchEpisodeSources(parent.id)
-    return data.sources.map((source) => ({
-      id: `${parent.id}-${source.quality}`,
-      url: source.url,
-      quality: source.quality,
-      isM3U8: source.isM3U8,
-      episodeId: parent.id,
-    }));
-  }
-}
 
 export default {
   Query,
-  Anime,
-  Episode,
-  Recent
 }
